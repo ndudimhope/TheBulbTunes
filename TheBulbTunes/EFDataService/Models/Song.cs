f3dd44f0a7f1083e0d339c32ee62b09f022e9eb9
Hope Ndudim <59125945+ndudimhope@users.noreply.github.com>
2021-08-16 13:30:32 +0100

    Define Song Entity

---
+++ TheBulbTunes/EFDataService/Models/Song.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBulbTunes.EFDataService.Models
+{
+   public class Song
+    {
+        [Required]
+        public Guid SongId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Artist { get; set; }
+
+
+        [Required, MaxLength(100)]
+        public string Album { get; set; }
+
+        [Required, MaxLength(250)]
+        public string Featuring { get; set; }
+
+        [MaxLength(100)]
+        public String Genre { get; set; }
+
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
