30f59ae359f0ed898c304a92f3c267c8d1cc09f2
Hope Ndudim <59125945+ndudimhope@users.noreply.github.com>
2021-08-16 13:31:06 +0100

    Define Favourite Entity

---
+++ TheBulbTunes/EFDataService/Models/Favourite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBulbTunes.EFDataService.Models
+{
+    public class Favourite
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid SongId { get; set; }
+
+
+        [Required]
+        public virtual Song SelectedSongId { get; set; }
+
+
+        //Navigation Properties
+        [Required]
+        public Guid AddedByUserId { get; set; }
+
+        [Required]
+        public virtual User AddedBy { get; set; }
+    }
+}
