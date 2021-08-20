97bed45ddde19859c336dc7826ba626428104abd
Hope Ndudim <59125945+ndudimhope@users.noreply.github.com>
2021-08-20 06:17:12 +0100

    Implemented and Consumed Update and Delete Methods

--- TheBulbTunes/Program.cs
+++ TheBulbTunes/Program.cs
@@ -23,7 +23,7 @@ namespace TheBulbTunes
             //songService.Create("Essence", "Wizkid", "Made In Lagos", "Tems", "R&B", new DateTime(2020, 10, 30));
 
             //// Fetch all songs
-            //List<Song> availableSongs = songService.FetchAll();
+            List<Song> availableSongs = songService.FetchAll();
 
             //Console.Write("Title\tArtist\tAlbum");
             //foreach (Song song in availableSongs)
@@ -38,21 +38,73 @@ namespace TheBulbTunes
             //List<Song> filteredSongs3;
             //List<Song> filteredSongs4;
 
-            Console.WriteLine("\n\nFILTERED SONGS FOR JANE\n");
-            Console.Write("Title\t\tArtist\t\tAlbum");
-            foreach (Song song in filteredSongs1)
-            {
-                Console.WriteLine();
-                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
-            }
+            //Console.WriteLine("\n\nFILTERED SONGS FOR JANE\n");
+            //Console.Write("Title\t\tArtist\t\tAlbum");
+            //foreach (Song song in filteredSongs1)
+            //{
+            //    Console.WriteLine();
+            //    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+            //}
 
-            Console.WriteLine("\n\nFILTERED SONGS FOR HOPE\n");
-            Console.Write("Title\t\tArtist\t\tAlbum");
-            foreach (Song song in filteredSongs2)
+            //Console.WriteLine("\n\nFILTERED SONGS FOR HOPE\n");
+            //Console.Write("Title\t\tArtist\t\tAlbum");
+            //foreach (Song song in filteredSongs2)
+            //{
+            //    Console.WriteLine();
+            //    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+            //}
+
+
+            // Set the id of song to be updated
+            Guid idOfSongToUpdate1 = new Guid("D0E4740A-6D90-41BB-423C-08D963264A6B"); // Non-existing id
+            Guid idOfSongToUpdate2 = new Guid("D0E4740A-6D90-41BB-423C-08D963264A4B"); // Existing id
+
+            // Create a Song object containing new info for the update
+            Song songWithNewInfo = new Song()
             {
-                Console.WriteLine();
-                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
-            }
+                Genre = "Rap/Hip-hop",
+                ReleaseDate = new DateTime(2013, 3, 31)
+            };
+
+            // Call the Update method to make the desired update
+            songService.Update(idOfSongToUpdate1, songWithNewInfo);
+            songService.Update(idOfSongToUpdate2, songWithNewInfo);
+
+            Console.WriteLine("\n\nFILTERED SONGS FOR JANE\n");
+            // Fetch all songs after update
+            availableSongs = songService.FetchAll();
+
+            Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
+            Console.Write("Title\t\tArtist\t\tAlbum\t\tGenre");
+            
+            foreach (Song song in availableSongs)
+                {
+                    Console.WriteLine();
+                    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}\t{song.Genre}");
+                }
+
+          
+
+            // Set the id of song to be deleted
+            Guid idOfSongToDelete1 = new Guid("D0E4740A-6D90-41BB-423C-08D963264A7B"); // Non-existing id
+            Guid idOfSongToDelete2 = new Guid("D0E4740A-6D90-41BB-423C-08D963264A4B"); // Existing id
+
+            // Call the Delete method to perform the desired deletion
+            songService.Delete(idOfSongToDelete1);
+            songService.Delete(idOfSongToDelete2);
+
+            // Fetch all songs after delete
+            availableSongs = songService.FetchAll();
+
+            Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
+            Console.Write("Title\t\tArtist\t\tAlbum");
+            
+            foreach (Song song in availableSongs)
+                {
+                    Console.WriteLine();
+                    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+                }
+
 
         }
 
