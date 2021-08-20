b666a64fd32fb6f5f44eb6c0304740a2e662b6ec
Hope Ndudim <59125945+ndudimhope@users.noreply.github.com>
2021-08-20 08:40:05 +0100

    Implemented and Consumed FetchwithFilter Method

--- TheBulbTunes/EFDataService/Services/UserService.cs
+++ TheBulbTunes/EFDataService/Services/UserService.cs
@@ -35,6 +35,43 @@ namespace TheBulbTunes.EFDataService.Services
             return _context.Users.ToList();
         }
 
+        //Fetch with Filter
+        public List<User> FetchWithFilter(string firstNameFilter= null, string lastNameFilter= null, string emailAddressFilter= null)
+        {
+            _users = FetchAll();
+
+
+            // If any filter is specified, apply that filter by calling its search method
+
+            if (firstNameFilter != null)
+                _users = SearchByFirstName(firstNameFilter, _users);
+
+            if (lastNameFilter != null)
+                _users = SearchByFirstName(firstNameFilter, _users);
+
+            if (emailAddressFilter != null)
+                _users = SearchByFirstName(firstNameFilter, _users);
+
+            return _users;
+
+        }
+
+        // Private helper methods to simplify searching with various parameters
+
+        private List<User> SearchByFirstName(string searchValue, List<User> users)
+        {
+            return users.Where(u => u.FirstName.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        private List<User> SearchByLastName(string searchValue, List<User> users)
+        {
+            return users.Where(u => u.LastName.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        private List<User> SearchByEmailAddress(string searchValue, List<User> users)
+        {
+            return users.Where(u => u.EmailAddress.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
 
     }
 }
--- TheBulbTunes/Program.cs
+++ TheBulbTunes/Program.cs
@@ -11,99 +11,42 @@ namespace TheBulbTunes
     {
         static void Main(string[] args)
         {
+            UserService userService = new UserService();
 
+            ////Create Users
+            //userService.Create("Hope", "Casso","cassolagos@gmail.com");
+            //userService.Create("Tems", "Shantee", "shantee@gmail.com");
+            //userService.Create("Enoch", "Emeka", "nochnoch@gmail.com");
+            //userService.Create("Presh", "Ndudim", "presh@gmail.com");
+            //userService.Create("Cephas", "Ademosun", "cephas@gmail.com");
 
-            SongService songService = new SongService();
+            ////Fetch All Users
+            //List<User> availableUsers = userService.FetchAll();
 
-            //// Create a number of songs
-            //songService.Create("All Over", "Tiwa Savage", "Sugarcane", "", "Afro-pop, Romantic", new DateTime(2017, 5, 22));
-            //songService.Create("Nobody's Business", "Rihanna", "Unapologetic", "Chris Brown", "R&B", new DateTime(2012, 1, 1));
-            //songService.Create("Get Down On It", "Kool & The Gang", "Something Special", "", "Funk", new DateTime(1981, 11, 24));
-            //songService.Create("The Monster", "Eminem", "Marshall Matters", "Rihanna", "R&B/Rap", new DateTime(2013, 1, 1));
-            //songService.Create("Essence", "Wizkid", "Made In Lagos", "Tems", "R&B", new DateTime(2020, 10, 30));
-
-            //// Fetch all songs
-            List<Song> availableSongs = songService.FetchAll();
-
-            //Console.Write("Title\tArtist\tAlbum");
-            //foreach (Song song in availableSongs)
+            //Console.Write("FirstName\tLastNmae\tEmail");
+            //foreach (User user in availableUsers)
             //{
             //    Console.WriteLine();
-            //    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
-            //}
-
-            // Fetch songs that meet some search criteria
-            List<Song> filteredSongs1 = songService.FetchWithFilter("over", "romantic", null, null);
-            List<Song> filteredSongs2 = songService.FetchWithFilter("Ess", "R", "Lagos", "Kid");
-            //List<Song> filteredSongs3;
-            //List<Song> filteredSongs4;
-
-            //Console.WriteLine("\n\nFILTERED SONGS FOR JANE\n");
-            //Console.Write("Title\t\tArtist\t\tAlbum");
-            //foreach (Song song in filteredSongs1)
-            //{
-            //    Console.WriteLine();
-            //    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
-            //}
-
-            //Console.WriteLine("\n\nFILTERED SONGS FOR HOPE\n");
-            //Console.Write("Title\t\tArtist\t\tAlbum");
-            //foreach (Song song in filteredSongs2)
-            //{
-            //    Console.WriteLine();
-            //    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+            //    Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
             //}
 
 
-            // Set the id of song to be updated
-            Guid idOfSongToUpdate1 = new Guid("D0E4740A-6D90-41BB-423C-08D963264A6B"); // Non-existing id
-            Guid idOfSongToUpdate2 = new Guid("D0E4740A-6D90-41BB-423C-08D963264A4B"); // Existing id
+            //Fetch Users that meet Filter Criteria
+            List<User> filteredUser1 = userService.FetchWithFilter("Hop", "ss", null);
+            List<User> filteredUser2 = userService.FetchWithFilter("noc", "mek", "noch");
 
-            // Create a Song object containing new info for the update
-            Song songWithNewInfo = new Song()
-            {
-                Genre = "Rap/Hip-hop",
-                ReleaseDate = new DateTime(2013, 3, 31)
-            };
-
-            // Call the Update method to make the desired update
-            songService.Update(idOfSongToUpdate1, songWithNewInfo);
-            songService.Update(idOfSongToUpdate2, songWithNewInfo);
-
-            Console.WriteLine("\n\nFILTERED SONGS FOR JANE\n");
-            // Fetch all songs after update
-            availableSongs = songService.FetchAll();
-
-            Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
-            Console.Write("Title\t\tArtist\t\tAlbum\t\tGenre");
-            
-            foreach (Song song in availableSongs)
-                {
-                    Console.WriteLine();
-                    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}\t{song.Genre}");
-                }
-
+            Console.WriteLine("\n\n FILTERED USER1\n");
           
 
-            // Set the id of song to be deleted
-            Guid idOfSongToDelete1 = new Guid("D0E4740A-6D90-41BB-423C-08D963264A7B"); // Non-existing id
-            Guid idOfSongToDelete2 = new Guid("D0E4740A-6D90-41BB-423C-08D963264A4B"); // Existing id
+            foreach (User user in filteredUser1)
+            {
 
-            // Call the Delete method to perform the desired deletion
-            songService.Delete(idOfSongToDelete1);
-            songService.Delete(idOfSongToDelete2);
 
-            // Fetch all songs after delete
-            availableSongs = songService.FetchAll();
+                Console.Write("FirstName\tLastNmae\tEmail");
+                Console.WriteLine();
+                Console.Write($"{user.FirstName}\t{user.LastName}\t{user.EmailAddress}");
 
-            Console.WriteLine("\n\nCURRENTLY AVAILABLE SONGS:\n");
-            Console.Write("Title\t\tArtist\t\tAlbum");
-            
-            foreach (Song song in availableSongs)
-                {
-                    Console.WriteLine();
-                    Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
-                }
+            }
 
 
         }
