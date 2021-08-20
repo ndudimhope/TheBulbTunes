c9b172eabebeef43cb4e0cc35fd880cc4ebc50af
Hope Ndudim <59125945+ndudimhope@users.noreply.github.com>
2021-08-20 06:58:29 +0100

    Implemented Create and FetchAll Methods

---
+++ TheBulbTunes/EFDataService/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheBulbTunes.EFDataService.Models;
+
+namespace TheBulbTunes.EFDataService.Services
+{
+    class UserService
+    {
+        private readonly AppDbContext _context = new AppDbContext();
+        private List<User> _users;
+
+
+        // Create a song
+        public void Create(string firstName, string lastName, string emailAddress)
+        {
+            User newUser = new User()
+            {
+                UserId = new Guid(),
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = emailAddress
+
+            };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+
+        }
+
+        //Fetch All Users
+        public List<User> FetchAll()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
